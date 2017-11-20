a1177863776b14314c807f63217eecbf39f8448b
andyfeng <andyfeng@hku.hk>
2017-11-20 22:43:43 +0800

    registration complete

--- TP/tutoria/views/log_view.py
+++ TP/tutoria/views/log_view.py
@@ -2,11 +2,15 @@ from django.shortcuts import render_to_response
 from django.contrib import auth
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+import re
 #import from self defined packages
 from .student_view import homepage_view as student_homepage_view
 from .tutor_view import homepage_view as tutor_homepage_view
 from .both_view import homepage_view as both_homepage_view
-from ..models import Tutor, Student
+from ..models import Tutor, Student, TutorProfile
+
+validReg = True
+error_message = ""
 
 #select login type
 def typeSelect(id):   
@@ -23,7 +27,7 @@ def typeSelect(id):
             return 'Both'
         else:
             return 'Student'
-    else:
+    elif isTutor:
         return 'Tutor'
 
 def login(request):
@@ -40,51 +44,118 @@ def login(request):
                 return both_homepage_view.homepage(request)
         #simply request login page
         else:
-            return render_to_response('tutoria/login.html')
+            return render_to_response('tutoria/login.html',{'wrong_password':False, 'invalid_user':False})
     #if POST method
     else:
         username = request.POST['username']
         password = request.POST['password']
-        user = auth.authenticate(username=username, password=password)
-        if user is not None and user.is_active:
-            user_type = typeSelect(user.id)
-            auth.login(request,user)
-            if user_type == 'Student':               
-                return student_homepage_view.homepage(request)
-            elif user_type == 'Tutor':
-                return tutor_homepage_view.homepage(request)
-            elif user_type == 'Both':
-                return both_homepage_view.homepage(request)
+        if User.objects.filter(username=username).exists():
+            user = auth.authenticate(username=username, password=password)
+            if user is not None and user.is_active:
+                user_type = typeSelect(user.id)
+                auth.login(request,user)
+                if user_type == 'Student':               
+                    return student_homepage_view.homepage(request)
+                elif user_type == 'Tutor':
+                    return tutor_homepage_view.homepage(request)
+                elif user_type == 'Both':
+                    return both_homepage_view.homepage(request)
+            else:
+                return render_to_response('tutoria/login.html',{'wrong_password':True, 'invalid_user':False})
         else:
-            return render_to_response('tutoria/login.html',{'wrong_password': True})
+            return render_to_response('tutoria/login.html', {'wrong_password':False, 'invalid_user':True})
 
 def registrate(request):
+    global error_message
+    error_message = ""
     if request.method == 'POST':
         #create a user
         username = request.POST['username']
+        usernameCheck(username)
+        
         password = request.POST['password']
+        confirmpassword = request.POST['confirmpassword']
+        passwordCheck(password, confirmpassword)
+        
         firstname = request.POST['firstname']
         lastname = request.POST['lastname']
         email = request.POST['email']
-        user = User.objects.create_user(
-            username=username, password=password, email=email, first_name=firstname, last_name=lastname
-        )
+        if validReg:
+            user = User.objects.create_user(
+                username=username, password=password, email=email, first_name=firstname, last_name=lastname
+            )
+        else:
+            university = TutorProfile.objects.values_list('university',flat=True).distinct()
+            return render_to_response('tutoria/registration.html', {'error_message': error_message, 'flag':True, 'university_list':university})
         #create a corresponding type object
         usr_type = request.POST['type']
         if usr_type == 'student':       
             Student.objects.create(user=user, login_type='Student')
         elif usr_type == 'tutor':
-            Tutor.objects.create(user=user, login_type='Tutor')
+            university = request.POST['university']
+            tutortype = request.POST['tutortype']
+            if tutortype == 'private':
+                hourly_rate = request.POST['hourlyrate']
+                tempProfile = TutorProfile.objects.create(university=university, hourly_rate=hourly_rate)
+            else:
+                tempProfile = TutorProfile.objects.create(university=university, hourly_rate=0)
+            Tutor.objects.create(user=user, login_type='Tutor', tutor_type=tutortype, profile=tempProfile)
         elif usr_type == 'both':
             Student.objects.create(user=user, login_type='Student')
-            Tutor.objects.create(user=user, login_type='Tutor')
+            university = request.POST['university']
+            tutortype = request.POST['tutortype']
+            if tutortype == 'private':
+                hourly_rate = request.POST['hourlyrate']
+                tempProfile = TutorProfile.objects.create(university=university, hourly_rate=hourly_rate)
+            else:
+                tempProfile = TutorProfile.objects.create(university=university, hourly_rate=0)
+            Tutor.objects.create(user=user, login_type='Tutor', tutor_type=tutortype, profile=tempProfile)
 
-        return render_to_response('tutoria/login.html')
+        return render_to_response('tutoria/login.html',{'wrong_password':False, 'invalid_user':False})
     # if request registration
     else:
-        return render_to_response('tutoria/registration.html')
+        university = TutorProfile.objects.values_list('university',flat=True).distinct()
+        return render_to_response('tutoria/registration.html',{'university_list':university})
 
 @login_required
 def logout(request):
     auth.logout(request)
     return render_to_response('tutoria/login.html')
+
+def usernameCheck(username):
+    global error_message
+    global validReg
+    if re.match("^[a-zA-Z0-9_-]+$", username) is None:
+        error_message += "invalid username, "
+        validReg = False
+
+    if User.objects.filter(username=username).exists():
+        error_message += "duplicate username, "
+        validReg = False
+
+def passwordCheck(password,confirmpassword):
+    global error_message
+    global validReg
+    if len(password) < 8:
+        error_message += "password length less than 8, "
+        validReg = False
+    
+    if re.search(r"\d", password) is None:
+        error_message += "password has no digit, "
+        validReg = False
+    
+    if re.search(r"[A-Z]", password) is None:
+        error_message += "password has no uppercase letter, "
+        validReg = False
+    
+    if re.search(r"[a-z]", password) is None:
+        error_message += "password has no lowercase letter, "
+        validReg = False
+    
+    if re.search(r"[ !#$%&'()*+,-./[\\\]^_`{|}~"+r'"]', password) is not None:
+        error_message += "password has invalid symbol, "
+        validReg = False
+    
+    if not password == confirmpassword:
+        error_message += "password doesnot match with confirmpassword."
+        validReg = False
